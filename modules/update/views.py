08d1355d0aa6a289b1e90e763492a5daa306d48c
John Martin <john.martin@cleardata.com>
2016-07-27 19:43:27 -0500

    updated docstrings

--- modules/update/views.py
+++ modules/update/views.py
@@ -8,6 +8,12 @@ class Update(object):
         self.args = kwargs
 
     def pull(self, branch="master"):
+        """
+        Update your application to the desired version!
+
+        :PARAM: STRING : branch :: DEFAULT=master : This determines which
+                    version of the core framework will be running.
+        """
         command = "git fetch"
         self.runCommand(command)
         command = "git checkout {0}".format(branch)
@@ -16,11 +22,20 @@ class Update(object):
         self.runCommand(command)
 
     def runCommand(self, command):
+        """
+        Exec a shell command via subprocess.Popen
+
+        :PARAM: STRING : command :: Full shell command
+        """
         command = command.split()
         process = subprocess.Popen(command, cwd=self.args['program_path'])
         process.wait()
 
     def run(self):
+        """
+        Since this module is pretty simple at the moment, we're just doing a
+        self.pull()
+        """
         self.pull(
                 branch=self.args['branch']
                 )
